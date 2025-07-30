454d537a702f25924a64220cdcc1b74a5fbfb5a9
wang <w.limin@qq.com>
2025-07-30 16:28:39 +0800

    支持在其他目录下创建运行容器
    Signed-off-by: wang <w.limin@qq.com>

--- cli.sh
+++ cli.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 创建容器或运行存在的容器，并进入交互命令行
-
+SHELL_FOLDER="$( dirname "${BASH_SOURCE[0]}" )"
 # 容器卷目录，保存下载的模型
-VOLUMES=$PWD/
+VOLUMES=$PWD
 
 #容器名，存在则启动，否则创建
 CONTAINER_NAME=chat-tts-forge-webui
@@ -11,7 +11,7 @@ CONTAINER_NAME=chat-tts-forge-webui
 # 检查专属网络是否创建，用于OpenWebui+ollama的语音交互。可以直接用容器名和端口作为主机名及端口进行内部通信，无需NAT路由
 DOCKER_NET=openwebui-net
 
-source ${VOLUMES}/common.sh
+source ${SHELL_FOLDER}/common.sh
 # 传递给容器的默认命令行
 CMD_ARG=
 if [ $NV_GPU -eq 0 ]; then #没有gpu支持
