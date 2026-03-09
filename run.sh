606492efbc0e90462826e1b24bcc2ea75bcd8373
wang <w.limin@qq.com>
2026-03-09 11:22:08 +0800

    for some transcribe
    Signed-off-by: wang <w.limin@qq.com>

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ EXTEND_ENV='-e LOG_LEVEL=ERROR'
 #EXTEND_ENV='-e LOG_LEVEL=DEBUG'
 
 cli_common
-docker logs -f $CONTAINER_NAME
+# docker logs -f $CONTAINER_NAME
 
 :<<'EOF'
 # python3 webui.py --api
